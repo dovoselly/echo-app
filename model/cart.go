c2cd8b04fa8d412112e5e9ac76d3e5bd2ed152a9
dovoselly <dovo@selly.vn>
2022-05-05 18:23:58 +0700

    model cart

--- model/cart.go
+++ model/cart.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type (
 	Cart struct {
 		ID        primitive.ObjectID `param:"id" json:"_id" bson:"_id"`
-		UserId    primitive.ObjectID `json:"user_id" bson:"user_id"`
+		UserId    primitive.ObjectID `json:"userId" bson:"userId"`
 		Items     []CartItem         `json:"items" bson:"items"`
 		CreatedAt string             `json:"createdAt" bson:"createdAt"`
 		UpdatedAt string             `json:"updatedAt" bson:"updatedAt"`
