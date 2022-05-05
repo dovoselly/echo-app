52323dfdce116dfbef6ad52344dc7fbcbbb50fe7
dovoselly <dovo@selly.vn>
2022-05-05 17:29:22 +0700

    model review

---
+++ model/review.go
@@ -0,0 +1,20 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type (
+	Review struct {
+		ID        primitive.ObjectID `json:"_id" bson:"_id"`
+		AdminId   primitive.ObjectID `json:"adminId" bson:"adminId"`
+		ReviewId  primitive.ObjectID `json:"reviewId" bson:"reviewId"`
+		Content   primitive.ObjectID `json:"content" bson:"content"`
+		CreatedAt string             `json:"createdAt" bson:"createdAt"`
+		UpdatedAt string             `json:"updatedAt" bson:"updatedAt"`
+	}
+
+	QueryReview struct {
+		Page      string `json:"page" bson:"page"`
+		Rating    uint   `json:"rating" bson:"rating"`
+		CreatedAt string `json:"createdAt" bson:"createdAt"`
+	}
+)
