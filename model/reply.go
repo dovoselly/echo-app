a26abb22d70d61beb68eb3f59b7820b272e4c4b4
dovoselly <dovo@selly.vn>
2022-05-05 17:44:45 +0700

    model reply

--- model/reply.go
+++ model/reply.go
@@ -1,1 +1,14 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type (
+	Reply struct {
+		ID        primitive.ObjectID `json:"_id" bson:"_id"`
+		AdminId   primitive.ObjectID `json:"adminId" bson:"adminId"`
+		ReviewId  primitive.ObjectID `json:"reviewId" bson:"reviewId"`
+		Content   string             `json:"content" bson:"content"`
+		CreatedAt string             `json:"createdAt" bson:"createdAt"`
+		UpdatedAt string             `json:"updatedAt" bson:"updatedAt"`
+	}
+)
