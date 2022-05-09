278b940db9d2e822945628582b6cd6b97347dfb2
dovoselly <dovo@selly.vn>
2022-05-09 13:25:09 +0700

    fix config jwt

--- routes/user.go
+++ routes/user.go
@@ -2,17 +2,15 @@ package routes
 
 import (
 	"echo-app/controllers"
-	"echo-app/middlewares"
-	"echo-app/validations"
 	"github.com/labstack/echo/v4"
 )
 
 func user(e *echo.Echo) {
-	e.POST("/register", controllers.Register, validations.Register)
+	e.POST("/register", controllers.Register)
 	e.POST("/login", controllers.Login)
-	e.PATCH("/password", controllers.ChangePassword, middlewares.Auth)
-	e.POST("/reset-password", controllers.ResetPassword, middlewares.Auth)
+	e.PATCH("/password", controllers.ChangePassword)
+	e.POST("/reset-password", controllers.ResetPassword)
 	e.GET("/:username", controllers.GetUserByUsername)
-	e.PUT("/me", controllers.UpdateUserInfo, middlewares.Auth)
-	e.PATCH("/me/change-avatar", controllers.ChangeAvatar, middlewares.Auth)
+	e.PUT("/me", controllers.UpdateUserInfo)
+	e.PATCH("/me/change-avatar", controllers.ChangeAvatar)
 }
