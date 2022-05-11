ee202415c37114c4e09b815a40eb435aa8a718e2
Ngô Thanh Tuấn <tuango.472000@gmail.com>
2022-05-11 17:42:26 +0700

    refactor code

--- routes/category.go
+++ routes/category.go
@@ -1,33 +1,20 @@
-package admin
-
-import (
-	"echo-app/config"
-	"echo-app/controllers"
-	"echo-app/validations"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-)
-
 package routes
 
 import (
-"echo-app/config"
-"echo-app/controllers"
-"echo-app/validations"
-"github.com/labstack/echo/v4"
-"github.com/labstack/echo/v4/middleware"
+	"echo-app/config"
+	"github.com/labstack/echo/v4"
 )
 
 var envVars = config.GetEnv()
 
 func category(e *echo.Echo) {
-	isLogin := middleware.JWT([]byte(envVars.Jwt.SecretKey))
-	categoryRouter := e.Group("/admin/categories", isLogin)
+	//isLogin := middleware.JWT([]byte(envVars.Jwt.SecretKey))
+	//categoryRouter := e.Group("/admin/categories", isLogin)
 	{
-		categoryRouter.POST("", controllers.CreateCategory)
-		categoryRouter.GET("", controllers.GetListCategory)
-		categoryRouter.GET("/:id", controllers.GetCategoryByID)
-		categoryRouter.PUT("/:id", controllers.UpdateCategory)
+		//categoryRouter.POST("", controllers.CreateCategory)
+		//categoryRouter.GET("", controllers.GetListCategory)
+		//categoryRouter.GET("/:id", controllers.GetCategoryByID)
+		//categoryRouter.PUT("/:id", controllers.UpdateCategory)
 		//categoryRouter.PATCH("/:id/disable", controllers.D )
 
 	}
