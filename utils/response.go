69d99209fcd49e54a1776f86937a205ce86ecba2
dovoselly <dovo@selly.vn>
2022-05-16 10:57:26 +0700

    fix response status 200

--- utils/response.go
+++ utils/response.go
@@ -19,7 +19,7 @@ func Response200(c echo.Context, data interface{}, message string) error {
 	if message == "" {
 		message = "Thanh Cong"
 	}
-	return c.JSON(http.StatusBadRequest, generateResponse(data, message))
+	return c.JSON(http.StatusOK, generateResponse(data, message))
 }
 
 // Response400 bad request
